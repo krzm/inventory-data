555ade31974e218418beb244d6aa194760c66bce
krzm <kmazanek@gmail.com>
2022-06-04 00:54:40 +0200

    Restrict max double

--- Inventory.Data/Model/Model.cs
+++ Inventory.Data/Model/Model.cs
@@ -5,6 +5,6 @@ public abstract class Model
     protected const int NameMaxLength = 70;
     protected const int DescriptionMaxLength = 160;
     protected const int PathMaxLength = 260;
-
+    protected const double MaxDoubleValue = 1000000;
     protected const string IdError = "Id must be greater than zero";
 }
